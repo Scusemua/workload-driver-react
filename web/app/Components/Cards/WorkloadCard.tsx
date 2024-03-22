5b7324ca078f2ed4d8abfdec58067c0465bef823
Scusemua <benrcarver@gmail.com>
2024-03-22 15:30:29 -0400

    Adjusted workloads card

--- web/app/Components/Cards/WorkloadCard.tsx
+++ web/app/Components/Cards/WorkloadCard.tsx
@@ -166,23 +166,15 @@ export const WorkloadCard: React.FunctionComponent<WorkloadCardProps> = (props: 
     };
 
     return (
-        <Card isRounded isFullHeight isExpanded={isCardExpanded}>
-            <CardHeader
-                height={1400}
-                actions={{ actions: cardHeaderActions, hasNoOffset: true }}
-                onExpand={onCardExpand}
-                toggleButtonProps={{
-                    id: 'expand-workloads-button',
-                    'aria-label': 'expand-workloads-button',
-                    'aria-expanded': isCardExpanded,
-                }}
-            >
+        <Card isRounded isFullHeight>
+            <CardHeader height={1400} actions={{ actions: cardHeaderActions, hasNoOffset: true }}>
                 <Title headingLevel="h1" size="xl">
                     Workloads
                 </Title>
             </CardHeader>
-            <CardExpandableContent>
-                <CardBody>
+            <CardBody>
+                {workloads.length == 0 && <Text component={TextVariants.h2}>There are no registered workloads.</Text>}
+                {workloads.length >= 1 && (
                     <DataList
                         isCompact
                         aria-label="data list"
@@ -490,40 +482,41 @@ export const WorkloadCard: React.FunctionComponent<WorkloadCardProps> = (props: 
                                 </DataListItem>
                             ))}
                     </DataList>
-                    <Pagination
-                        isDisabled={workloads.length == 0}
-                        itemCount={workloads.length}
-                        widgetId="bottom-example"
-                        perPage={perPage}
-                        page={page}
-                        variant={PaginationVariant.bottom}
-                        perPageOptions={[
-                            {
-                                title: '1',
-                                value: 1,
-                            },
-                            {
-                                title: '2',
-                                value: 2,
-                            },
-                            {
-                                title: '3',
-                                value: 3,
-                            },
-                            // {
-                            //     title: '4',
-                            //     value: 4,
-                            // },
-                            // {
-                            //     title: '5',
-                            //     value: 5,
-                            // },
-                        ]}
-                        onSetPage={onSetPage}
-                        onPerPageSelect={onPerPageSelect}
-                    />
-                </CardBody>
-            </CardExpandableContent>
+                )}
+                <Pagination
+                    hidden={workloads.length == 0}
+                    isDisabled={workloads.length == 0}
+                    itemCount={workloads.length}
+                    widgetId="bottom-example"
+                    perPage={perPage}
+                    page={page}
+                    variant={PaginationVariant.bottom}
+                    perPageOptions={[
+                        {
+                            title: '1',
+                            value: 1,
+                        },
+                        {
+                            title: '2',
+                            value: 2,
+                        },
+                        {
+                            title: '3',
+                            value: 3,
+                        },
+                        // {
+                        //     title: '4',
+                        //     value: 4,
+                        // },
+                        // {
+                        //     title: '5',
+                        //     value: 5,
+                        // },
+                    ]}
+                    onSetPage={onSetPage}
+                    onPerPageSelect={onPerPageSelect}
+                />
+            </CardBody>
         </Card>
     );
 };
