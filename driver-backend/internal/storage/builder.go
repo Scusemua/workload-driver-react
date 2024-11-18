113e45cd9148fc94ac617b3bbf0656b0b13dbd90
bcarver <bcarver@gmu.edu>
2024-11-18 18:22:38 -0500

    Just use protobuf struct for RemoteStorage

--- driver-backend/internal/storage/builder.go
+++ driver-backend/internal/storage/builder.go
@@ -1,14 +1,18 @@
 package storage
 
+import (
+	"github.com/scusemua/workload-driver-react/m/v2/internal/server/api/proto"
+)
+
 // RemoteStorageBuilder is a builder for constructing a RemoteStorage object.
 type RemoteStorageBuilder struct {
 	name                         string
 	downloadRate                 int64
 	uploadRate                   int64
-	downloadVariancePercent      float64
-	uploadVariancePercent        float64
-	readFailureChancePercentage  float64
-	writeFailureChancePercentage float64
+	downloadVariancePercent      float32
+	uploadVariancePercent        float32
+	readFailureChancePercentage  float32
+	writeFailureChancePercentage float32
 }
 
 // NewRemoteStorageBuilder creates and returns a new instance of RemoteStorageBuilder.
@@ -35,32 +39,32 @@ func (b *RemoteStorageBuilder) WithUploadRate(rate int64) *RemoteStorageBuilder 
 }
 
 // WithDownloadVariancePercent sets the download variance percentage of the RemoteStorage.
-func (b *RemoteStorageBuilder) WithDownloadVariancePercent(percent float64) *RemoteStorageBuilder {
+func (b *RemoteStorageBuilder) WithDownloadVariancePercent(percent float32) *RemoteStorageBuilder {
 	b.downloadVariancePercent = percent
 	return b
 }
 
 // WithUploadVariancePercent sets the upload variance percentage of the RemoteStorage.
-func (b *RemoteStorageBuilder) WithUploadVariancePercent(percent float64) *RemoteStorageBuilder {
+func (b *RemoteStorageBuilder) WithUploadVariancePercent(percent float32) *RemoteStorageBuilder {
 	b.uploadVariancePercent = percent
 	return b
 }
 
 // WithReadFailureChancePercentage sets the read failure chance percentage of the RemoteStorage.
-func (b *RemoteStorageBuilder) WithReadFailureChancePercentage(percent float64) *RemoteStorageBuilder {
+func (b *RemoteStorageBuilder) WithReadFailureChancePercentage(percent float32) *RemoteStorageBuilder {
 	b.readFailureChancePercentage = percent
 	return b
 }
 
 // WithWriteFailureChancePercentage sets the write failure chance percentage of the RemoteStorage.
-func (b *RemoteStorageBuilder) WithWriteFailureChancePercentage(percent float64) *RemoteStorageBuilder {
+func (b *RemoteStorageBuilder) WithWriteFailureChancePercentage(percent float32) *RemoteStorageBuilder {
 	b.writeFailureChancePercentage = percent
 	return b
 }
 
 // Build constructs and returns a RemoteStorage object.
-func (b *RemoteStorageBuilder) Build() RemoteStorage {
-	return RemoteStorage{
+func (b *RemoteStorageBuilder) Build() *proto.RemoteStorageDefinition {
+	return &proto.RemoteStorageDefinition{
 		Name:                         b.name,
 		DownloadRate:                 b.downloadRate,
 		UploadRate:                   b.uploadRate,
