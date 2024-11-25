d011c3cfa355597ee2b6890888d47cccc158c364
bcarver <bcarver@gmu.edu>
2024-11-24 22:49:38 -0500

    Added nested training task tale

---
+++ driver-frontend/src/Components/Tables/SessionTableExpandedContent.tsx
@@ -0,0 +1,9 @@
+import { Session } from '@src/Data';
+
+interface ISessionTableExpandedContentProps {
+  session: Session;
+}
+
+const SessionTableExpandedContent: React.FunctionComponent<ISessionTableExpandedContentProps> = (props: ISessionTableExpandedContentProps) => {
+  
+}
