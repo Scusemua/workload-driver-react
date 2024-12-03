39f586b4f956e3ada1a4c6199f377f88901a1536
bcarver <bcarver@gmu.edu>
2024-12-02 19:39:22 -0500

    Changed the zap encoder helpers back to use regular request traces

--- driver-backend/internal/server/api/proto/request_trace.go
+++ driver-backend/internal/server/api/proto/request_trace.go
@@ -2,29 +2,29 @@ package proto
 
 import "go.uber.org/zap/zapcore"
 
-func (x *RequestTraceUpdated) MarshalLogObject(encoder zapcore.ObjectEncoder) error {
+func (x *RequestTrace) MarshalLogObject(encoder zapcore.ObjectEncoder) error {
 	encoder.AddString("message_id", x.MessageId)
 	encoder.AddString("message_type", x.MessageType)
 	encoder.AddString("kernel_id", x.KernelId)
-	err := encoder.AddArray("traces", TracesArr(x.Traces))
-	if err != nil {
-		return err
-	}
-	//encoder.AddInt64("gateway_received_request", x.GetRequestReceivedByGateway())
-	//encoder.AddInt64("gateway_forwarded_request", x.GetRequestSentByGateway())
-	//encoder.AddInt64("local_daemon_received_request", x.GetRequestReceivedByLocalDaemon())
-	//encoder.AddInt64("local_daemon_sent_request", x.GetRequestSentByLocalDaemon())
-	//encoder.AddInt64("kernel_replica_received_request", x.GetRequestReceivedByKernelReplica())
-	//encoder.AddInt64("kernel_replica_sent_reply", x.GetReplySentByKernelReplica())
-	//encoder.AddInt64("local_daemon_received_reply", x.GetReplyReceivedByLocalDaemon())
-	//encoder.AddInt64("local_daemon_sent_reply", x.GetReplySentByLocalDaemon())
-	//encoder.AddInt64("gateway_received_reply", x.GetReplyReceivedByGateway())
-	//encoder.AddInt64("gateway_forwarded_reply", x.GetReplySentByGateway())
+	//err := encoder.AddArray("traces", TracesArr(x.Traces))
+	//if err != nil {
+	//	return err
+	//}
+	encoder.AddInt64("gateway_received_request", x.GetRequestReceivedByGateway())
+	encoder.AddInt64("gateway_forwarded_request", x.GetRequestSentByGateway())
+	encoder.AddInt64("local_daemon_received_request", x.GetRequestReceivedByLocalDaemon())
+	encoder.AddInt64("local_daemon_sent_request", x.GetRequestSentByLocalDaemon())
+	encoder.AddInt64("kernel_replica_received_request", x.GetRequestReceivedByKernelReplica())
+	encoder.AddInt64("kernel_replica_sent_reply", x.GetReplySentByKernelReplica())
+	encoder.AddInt64("local_daemon_received_reply", x.GetReplyReceivedByLocalDaemon())
+	encoder.AddInt64("local_daemon_sent_reply", x.GetReplySentByLocalDaemon())
+	encoder.AddInt64("gateway_received_reply", x.GetReplyReceivedByGateway())
+	encoder.AddInt64("gateway_forwarded_reply", x.GetReplySentByGateway())
 
 	return nil
 }
 
-type RequestTraceArr []*RequestTraceUpdated
+type RequestTraceArr []*RequestTrace
 
 func (r RequestTraceArr) MarshalLogArray(encoder zapcore.ArrayEncoder) error {
 	for _, v := range r {
